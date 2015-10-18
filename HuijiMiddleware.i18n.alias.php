b0fb275820560916bcd2aa5d819693655cc8adee
谷溪 <guxi99@gmail.com>
2015-10-18 15:24:54 +0800

    Update HuijiMiddleware.i18n.alias.php

--- HuijiMiddleware.i18n.alias.php
+++ HuijiMiddleware.i18n.alias.php
@@ -13,11 +13,6 @@ $specialPageAliases['en'] = array(
 	'InvitationCode' => array( 'InvitationCode' ),
 );
 
-/** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'HelloWorld' => array( 'HalloWereld' ),
-);
-
 /** chinese */
 $specialPageAliases['zh-hans'] = array(
 	'InvitationCode' => array( '邀请码' ),
