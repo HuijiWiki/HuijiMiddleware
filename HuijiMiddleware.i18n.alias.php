dfed4cfed5f05118b50b0e8f170d4234f303940e
Reasno <guxi99@gmail.com>
2015-12-09 18:26:18 +0800

    Update HuijiMiddleware.i18n.alias.php

--- HuijiMiddleware.i18n.alias.php
+++ HuijiMiddleware.i18n.alias.php
@@ -11,13 +11,16 @@ $specialPageAliases = array();
 /** English (English) */
 $specialPageAliases['en'] = array(
 	'InvitationCode' => array( 'Invitation Code' ),
+	'invitationcode' => array( 'Invitation Code' ),
 );
 
 /** chinese */
 $specialPageAliases['zh-hans'] = array(
 	'InvitationCode' => array( '邀请码' ),
+	'invitationcode' => array( '邀请码' ),
 );
 /** chinese */
 $specialPageAliases['zh-cn'] = array(
 	'InvitationCode' => array( '邀请码' ),
+	'invitationcode' => array( '邀请码' ),
 );
