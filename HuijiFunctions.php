a9ae15245bdb4796b4fc773a55c1b7fa6cdddaf6
Reasno <guxi99@gmail.com>
2016-02-24 18:29:43 +0800

    Update HuijiFunctions.php

--- HuijiFunctions.php
+++ HuijiFunctions.php
@@ -92,7 +92,7 @@ Class HuijiFunctions {
 		if( $isURL && !$image && strlen( $linkText ) > 30 ) {
 			$start = substr( $linkText, 0, ( 30 / 2 ) - 3 );
 			$end = substr( $linkText, strlen( $linkText ) - ( 30 / 2 ) + 3, ( 30 / 2 ) - 3 );
-			$linkText = trim( $start ) . wfMsg( 'ellipsis' ) . trim( $end );
+			$linkText = trim( $start ) . wfMessage( 'ellipsis' )->plain() . trim( $end );
 		}
 		return $tagOpen . $linkText . $tagClose;
 	}
