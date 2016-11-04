91a980057623615fe23446a143cf948941079b60
Reasno <guxi99@gmail.com>
2016-11-05 00:30:42 +0800

    change parameters"

--- HuijiPageInfo.php
+++ HuijiPageInfo.php
@@ -30,9 +30,9 @@ class HuijiPageInfo extends ContextSource {
 			$tagScore = round($pageCounts['tag'] /($pageCounts['length']) * 2500 );
 			$strScore = $pageCounts['structure'] * 50;
 			$filScore = round($pageCounts['files']) * 2;
-			if ($pageCounts['length']/$pageCounts['comma'] < 300){
+			if ($pageCounts['length']/$pageCounts['comma'] < 100){
 				$comScore = 1;
-			} else if ($pageCounts['length'] / $pageCounts['comma'] < 3000) {
+			} else if ($pageCounts['length'] / $pageCounts['comma'] < 500) {
 				$comScore = 0.5;
 			} else {
 				$comScore = 0.1;
